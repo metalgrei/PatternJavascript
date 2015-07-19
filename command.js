03b3a3e7daa20114280a0e7c35c0d71bd0737def
metalgrei <isaac.etsia@gmail.com>
2015-07-19 17:23:50 +0200

    Add new example pattern

--- command.js
+++ command.js
@@ -1,3 +1,26 @@
 /**
  * Example pattern Command
  */
+(function () {
+	var CarManager = {
+		requestInfo : function (model, id) {
+			return 'The information for ' + model +
+			'with ID ' + id + 'is foobar';
+		},
+		buyVehicle : function (model, id) {
+			return 'You have successfully purchased Item '
+			 + id + ', a' + model;
+		},
+		arrangeViewing : function (model, id) {
+			return 'You have successfully booked a viewing of '
+			 + model + '(' + id + ')';
+		}
+
+	};
+
+	CarManager.execute = function (name) {
+		return CarManager[name] && CarManager[name].apply(CarManager, [].slice.call(arguments, 1));
+	};
+
+}
+	())
