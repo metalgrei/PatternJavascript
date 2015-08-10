606b57e4284f178a61f62c3170302def0c8be855
Isaac Mayoral Orellana <isaac.mayoral@gmail.com>
2015-08-10 11:52:56 +0200

    fto: add pattern command

--- command.js
+++ command.js
@@ -1,26 +1,31 @@
 /**
  * Example pattern Command
  */
-(function () {
+	())
+=======
+ (function(){
+	'use strict';
 	var CarManager = {
-		requestInfo : function (model, id) {
+		// request information
+		requestInfo:function(model, id){
 			return 'The information for ' + model +
-			'with ID ' + id + 'is foobar';
+			' with ID ' + id + ' is foobar';
 		},
-		buyVehicle : function (model, id) {
-			return 'You have successfully purchased Item '
-			 + id + ', a' + model;
+		//purchase the car
+		buyVehicle:function(model, id){
+			return 'You have successfully purchased Item ' 
+			+ id + ' , a ' + model;
 		},
-		arrangeViewing : function (model, id) {
+		arrangeViewing:function(model, id){
 			return 'You have successfully booked a viewing of '
-			 + model + '(' + id + ')';
+			+ model + ' ( ' + id + ' ) ';
 		}
-
 	};
-
+	
 	CarManager.execute = function (name) {
 		return CarManager[name] && CarManager[name].apply(CarManager, [].slice.call(arguments, 1));
 	};
-
-}
-	())
+	
+	CarManager.execute("buyVehicle", "Ford Escort", "453543");
+	
+}());
